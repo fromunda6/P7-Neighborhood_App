a5f5e9282d1213c8935d9f15c0bd37a077dced26
Goyo <fromunda6@zoho.com>
2016-12-01 18:09:18 -0700

    content, style and interactivity changes

--- README.md
+++ README.md
@@ -3,13 +3,26 @@ Udacity Front-End Web Developer Nanodegree (2016)
 Project #7
 
 ##Neighborhood Map Project
+-This is an application intended to 1) give users an overview of a particular neighborhood, for
+any purpose but especially geared towards prospective immigrants to the area, including a) places 
+of interest, b) "what's in the news?",special/honorable mention -type anecdotal info, c) local laws 
+and mores restricting/discouraging shady behavior, etc.
+
+##Functionally/Specifically
+-This will be achieved through a number of methods, but required at the minimum will be 1) a user 
+input form that will communicate with an embedded map API displaying the location at various levels
+of detail and marked with places of interest therein, a summary of news/events/happenings taken
+from local or national news outlets, 
 
 ##To-Do:
 
 -Don't forget to Modify package.json and gruntfile.js for the particulars of this project 
--Set up GitRepo
+-alt= info on img tag
 -learn/must use Knockout.js
+-
 
 #Floaters
+
+
 
 
--- js/script.js
+++ js/script.js
@@ -1,44 +1,18 @@
-//define, encapsulate, ___?, DOM variables as jquery-dynamic:
-	var $form = $('#form-container');
 
-	function loadData() {
-		var $body = $('body');
-		var $wikiElem = $('#wikipedia-links');
-		var $nytHeaderElem = $('#nytimes-header');
-		var $nytElem = $('#nytimes-articles');
-		var $greeting = $('#greeting');
-		var $flashyVAR = $('#flashyBOX');
+function loadData(){
 
-		//clear out old data before new request by setting these values to an empty string
-      	$wikiElem.text("");
-      	$nytElem.text("");
+var $body = $('body');
+var street = $('#street').val();
+var city = $('#city').val();
+var address = street + ', ' + city;
 
-    var $cityString=$('#street').val(); //collect the value of DOM element with id=street, in this case a form input
-		var $streetString=$('#city').val(); //" " " "
-		var address = $streetString + ', ' + $cityString;
+alert("So you want to live at " + address + '?');
 
-		$greeting.text('So you want to live at ' + address + '?');
+var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+$body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
-		// var streetviewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+return false;
 
-		// $body.append(streetviewURL);
-	};
+};
 
-//define functions
-    function initMap() {
-      var focus = {lat: 39.74, lng: -104.99};
-      var map = new google.maps.Map($('#map').get(0), {
-        zoom: 4,
-        center: focus
-      });
-      var marker = new google.maps.Marker({
-        position: focus,
-        map: map
-      });
-    };
-
-  // define JS-feature-containing DOM elements as jquery objects
-   var $body = $('body');
-
-  loadData();
-
+$('#form-container').submit(loadData);
