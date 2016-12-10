ae96b6caa6ce64b04e6c8117e43a715cf26e3752
Goyo <fromunda6@zoho.com>
2016-12-09 19:09:01 -0700

    continued work on catClicker
    taking my time w JS design patterns; got ahead of myself and stumbled

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
 
-function loadData(){
+// constraints/global objectives:
+// 1) minimize # of connections between application 'organs'
+// 2) coherently comment even slightly opaque code
+// 3) do not forgo testing
 
-var $body = $('body');
-var street = $('#street').val();
-var city = $('#city').val();
-var address = street + ', ' + city;
+var view = {
 
-alert("So you want to live at " + address + '?');
+	loadData: function(){
 
-var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
-$body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+		var $body = $('body');
+		var street = $('#street').val();
+		var city = $('#city').val();
+		var address = street + ', ' + city;
 
-return false;
+		alert("So you want to live at " + address + '?');
+
+		var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+		$body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+		return false;
+
+	};
+
+	$('#form-container').submit(loadData);
 
 };
 
-$('#form-container').submit(loadData);
+var model = {
+	//all location detail data here; all data here, in fact
+};
+
+var controller = {
+	//all interaction between model and view should exist here
+};
